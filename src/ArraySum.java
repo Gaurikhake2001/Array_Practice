0aed45f8a2df5dac8c7abcb3e776f7880a7031b2
Gaurikhake2001 <gaurikhake54321@gmail.com>
2024-05-30 10:34:09 +0530

    second

---
+++ src/ArraySum.java
@@ -0,0 +1,13 @@
+public class ArraySum {
+
+    public static void main(String args[]) {
+        int a[] = {10,50,65,89,20};
+        int sum = 0;
+
+        for(int i:a)
+        {
+            sum += i;
+        }
+        System.out.println("Sum of Elements in an Array: "+sum);
+    }
+}
